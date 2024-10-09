f562d9cf5d7152baaca5ebc7b3cc0b7d4e5bfa29
Felix Leyva <leyva@adesso-mobile.de>
2024-10-09 16:29:32 +0200

    develop: Added information for plugin publishing

--- libs-catalog-generator/build.gradle.kts
+++ libs-catalog-generator/build.gradle.kts
@@ -1,21 +1,23 @@
 plugins {
     `kotlin-dsl`
     `kotlin-dsl-precompiled-script-plugins`
-    id("com.gradle.plugin-publish") version "1.2.1"
+    id("com.gradle.plugin-publish") version "1.3.0"
 }
 group = "de.felixlf"
 version = "1.0"
 
 gradlePlugin {
     plugins {
+        vcsUrl = "https://github.com/felix-leyva/catalog-generator"
+        website = "https://github.com/felix-leyva/catalog-generator"
         create("libs-catalog-generator") {
             id = "de.felixlf.libs-catalog-generator"
             displayName = "Libs catalog generator for buildSrc and convention plugins"
+            tags = listOf("catalog", "generator", "buildSrc", "convention")
             description =
                 "This plugin generates a file to access from a buildSrc or convention plugin source files in a type-safe way, the libs, plugins and versions from a version catalog"
             implementationClass = "CatalogGeneratorPlugin"
         }
-
     }
 }
 
